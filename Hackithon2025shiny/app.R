eed902cda87ecd08c822a6622f4e381f44d155b0
Jan Kopejtko <jan.kopejtko@email.cz>
2025-05-23 20:03:57 +0200

    update (okres ukazka)

--- Hackithon2025shiny/app.R
+++ Hackithon2025shiny/app.R
@@ -8,44 +8,36 @@
 #
 
 library(shiny)
+library(leaflet)
+library(sf)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  titlePanel("Mapa okresů ČR z GeoJSON"),
+  leafletOutput("mapaCR", height = 600)
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  output$mapaCR <- renderLeaflet({
+    # Načti GeoJSON
+    okresy <- st_read("C:/Users/janko/Desktop/Hackithon/1/OKRESY_P.shp.geojson", quiet = TRUE)
+    
+    # Převod na GPS souřadnice
+    okresy <- st_transform(okresy, 4326)
+    
+    # Vykresli mapu
+    leaflet(okresy) %>%
+      addTiles() %>%
+      addPolygons(
+        fillColor = "red",
+        color = "darkgreen",
+        weight = 1,
+        fillOpacity = 0.5,
+        popup = ~NAZEV  # ← správný sloupec pro název okresu
+      ) %>%
+      setView(lng = 15.5, lat = 49.8, zoom = 7)
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
+
